8a3de95ec0b518ee293d27d54f6b3e9258fa63f0
yukyduky <yukyduky@users.noreply.github.com>
2018-01-24 10:28:08 +0100

    SoundManager WIP

--- LittleGame/SoundManager.h
+++ LittleGame/SoundManager.h
@@ -4,26 +4,40 @@
 
 #include <string>
 #include <SFML\Audio.hpp>
-#include <vector>
+#include <array>
+#include <deque>
 
 namespace SOUND {
-	enum NAME { GUNSHOT };
+	enum NAME { GUNSHOT, SIZE };
 }
 namespace MUSIC {
-	enum NAME { INGAME };
+	enum NAME { INGAME, SIZE };
 }
 
 class SoundManager
 {
 private:
 	bool repeatMusic;
-	
+	std::array<sf::SoundBuffer, SOUND::SIZE> sounds;
+	std::array<std::string, MUSIC::SIZE> musicFilenames;
+	std::deque<sf::Sound> soundQueue;
+	sf::Music currentMusic;
 public:
-	void loadSound(std::string filename);
-	void loadMusic(std::string filename);
-	void playSound(SOUND::NAME name);
-	void playMusic(MUSIC::NAME name);
-	void setRepeatMusic(bool repeat);
+	virtual int loadSound(std::string filename, SOUND::NAME name);
+	virtual int loadMusic(std::string filename);
+	virtual int mapMusic(std::string filename, MUSIC::NAME name);
+	virtual void update();
+	virtual void play(MUSIC::NAME name);
+	virtual void play(SOUND::NAME name);
+	virtual void stop(MUSIC::NAME name);
+	virtual void stop(SOUND::NAME name);
+	virtual void pause(MUSIC::NAME name);
+	virtual void pause(SOUND::NAME name);
+	virtual void adjustMaster(size_t volume);
+	virtual void adjustMusic(size_t volume);
+	virtual void adjustEffects(size_t volume);
+
+	virtual void setRepeatMusic(bool repeat);
 };
 
 #endif // !SOUNDMANAGER_H
