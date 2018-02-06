76849dda7b7bdad0a7d0d0bc58d9f1b96f24b3f9
OliverGlandberger <oliverglandberger@gmail.com>
2018-02-06 11:45:38 +0100

    Saving progress before swapping branches.

--- LittleGame/QuadTree.cpp
+++ LittleGame/QuadTree.cpp
@@ -1,24 +1,111 @@
-//#include "QuadTree.h"
+#include "QuadTree.h"
+
+QuadTreeSquare::QuadTreeSquare() {
+	this->widthLength = 0;
+	this->heightLength = 0;
+	this->x = 0;
+	this->y = 0;
+}
+
+QuadTreeSquare::QuadTreeSquare(float widthLength_in, float heightLength_in, float x_in, float y_in) {
+	this->widthLength = widthLength_in;
+	this->heightLength = heightLength_in;
+	this->x = x_in;
+	this->y = y_in;
+}
+
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+//                                     QUAD TREE           /
+///////////////////////////////////////////////////////////
+//////////////////////////////
+///////////////
+///////
 //
-//QuadTreeSquare::QuadTreeSquare() {
-//	this->sideLength = 0;
-//	this->x = 0;
-//	this->y = 0;
-//}
+
+/* _+_+_+_+_+_+_+_+_+_+_+_+_+_+_
+  |                             |
+  |           PRIVATE           |
+   -_-_-_-_-_-_-_-_-_-_-_-_-_-*/
+//_________________________________________//
+//                                         //
+//             END OF PRIVATE              //
+//_________________________________________//
+/////////////////////////////////////////////
+
+
+
+
+
+/* _+_+_+_+_+_+_+_+_+_+_+_+_+_+_
+  |                             |
+  |           PUBLIC            |
+   -_-_-_-_-_-_-_-_-_-_-_-_-_-*/
+
+QuadTree::QuadTree() {
+	this->level = 0;
+
+	this->nodes[BOTTOM_LEFT] = nullptr;
+	this->nodes[BOTTOM_RIGHT] = nullptr;
+	this->nodes[TOP_LEFT] = nullptr;
+	this->nodes[TOP_RIGHT] = nullptr;
+
+	this->parentPointer = nullptr;
+}
+
+QuadTree::QuadTree(
+	int level_in,
+	float widthLength_in, float heightLength_in,
+	float x_bottomLeftCorner, float y_bottomLeftCorner,
+	QuadTree* parentPointer_in)
+{
+	this->level = level_in;
+	this->nodeBounds.widthLength = widthLength_in;
+	this->nodeBounds.heightLength = heightLength_in;
+	this->nodeBounds.x = x_bottomLeftCorner;
+	this->nodeBounds.y = y_bottomLeftCorner;
+	this->parentPointer = parentPointer_in;
+}
+
+QuadTree::~QuadTree() {
+
+}
+
+void QuadTree::split() {
+	float tempWidth = (this->nodeBounds.widthLength * 0.5);
+	float tempHeight = (this->nodeBounds.heightLength * 0.5);
+
+	// Storing these so we don't have to 'get' them over and over
+	float tempX = this->nodeBounds.x;
+	float tempY = this->nodeBounds.y;
+
+	this->nodes[BOTTOM_LEFT] = new QuadTree(
+		(this->level + 1), tempWidth, tempHeight, tempX, tempY, this
+	);
+	this->nodes[BOTTOM_RIGHT] = new QuadTree(
+		(this->level + 1), tempWidth, tempHeight, (tempX + tempWidth), tempY, this
+	);
+	this->nodes[TOP_LEFT] = new QuadTree(
+		(this->level + 1), tempWidth, tempHeight, tempX, (tempY + tempHeight), this
+	);
+	this->nodes[TOP_RIGHT] = new QuadTree(
+		(this->level + 1), tempWidth, tempHeight, (tempX + tempWidth), (tempY + tempHeight), this
+	);
+}
+
+void QuadTree::cleanup() {
+
+}
+
+//_________________________________________//
+//                                         //
+//              END OF PUBLIC              //
+//_________________________________________//
+/////////////////////////////////////////////
+
 //
-// QuadTreeSquare::QuadTreeSquare(int sideLength_in, int x_in, int y_in) {
-// this->sideLength = sideLength_in;
-// this->x = x_in;
-// this->y = y_in;
-// }
-//
-//QuadTree::QuadTree(int level_in, int nodeSideLength, int x_bottomLeftCorner, int y_bottomLeftCorner) {
-//	this->level = level_in;
-//	this->nodeBounds.sideLength = nodeSideLength;
-//	this->nodeBounds.x = x_bottomLeftCorner;
-//	this->nodeBounds.y = y_bottomLeftCorner;
-//}
-//
-//QuadTree::~QuadTree() {
-//
-//}
+//\\\\\
+//\\\\\\\\\\\\\
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+//\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+//                                      QUAD TREE          \
+////////////////////////////////////////////////////////////
--- LittleGame/QuadTree.h
+++ LittleGame/QuadTree.h
@@ -1,44 +1,49 @@
-//#pragma once
-//#ifndef QUADTREE_HP
-//#define QUADTREE_HP
-//
-//#include "GameObject.h"
-//#include <list>
-//#include <d3d11.h>
-//
-//#define OBJECTS_MAX = 20
-//#define LAYERS_MAX = 5
-//
-//enum class QUADTREEINDEX { BOTTOM_LEFT, BOTTOM_RIGHT, TOP_LEFT, TOP_RIGHT };
-//
-//struct QuadTreeSquare {
-//	int sideLength;
-//	int x;
-//	int y;
-//	
-//	QuadTreeSquare();
-//	QuadTreeSquare(int sideLength_in, int x_in, int y_in);
-//};
-//
-//class QuadTree {
-//private:
-//	int level;
-//	std::list<GameObject*> staticObjectsList;
-//	std::list<GameObject*> dynamicObjectsList;
-//	QuadTreeSquare nodeBounds;
-//	QuadTree* nodes[4];
-//
-//public:
-//	QuadTree(int level_in, int nodeSideLength, int x_bottomLeftCorner, int y_bottomLeftCorner);
-//	~QuadTree();
-//
-//	void split();
-//	int getIndex();
-//	void insertObject();
-//	void removeObject();
-//
-//
-//	void cleanup();
-//};
-//
-//#endif
+#pragma once
+#ifndef QUADTREE_HP
+#define QUADTREE_HP
+
+#include "GameObject.h"
+#include <list>
+#include <d3d11.h>
+
+#define OBJECTS_MAX = 20
+#define LAYERS_MAX = 5
+
+struct QuadTreeSquare {
+	float widthLength;
+	float heightLength;
+	float x;
+	float y;
+
+	QuadTreeSquare();
+	QuadTreeSquare(float widthLength_in, float heightLength_in, float x_in, float y_in);
+};
+
+class QuadTree {
+private:
+	enum QUADTREEINDEX { BOTTOM_LEFT, BOTTOM_RIGHT, TOP_LEFT, TOP_RIGHT, SIZE };
+
+	int level;
+	std::list<GameObject*> staticObjectsList;
+	std::list<GameObject*> dynamicObjectsList;
+	QuadTreeSquare nodeBounds;
+	QuadTree* nodes[4];
+	QuadTree* parentPointer;
+
+public:
+	QuadTree();
+	QuadTree(int level_in, float widthLength_in, float heightLength_in, float x_bottomLeftCorner, float y_bottomLeftCorner, QuadTree* parentPointer_in);
+	~QuadTree();
+
+	void split();
+	void insertStaticObject(GameObject* staticObject);
+	void insertDynamicObject(GameObject* dynamicObject);
+	void removeStaticObject();
+	void removeDynamicObject();
+
+	int GETIndex();
+
+	void cleanup();
+};
+
+#endif
