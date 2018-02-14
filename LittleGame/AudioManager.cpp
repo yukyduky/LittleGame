cbee0e06ca6320886eeb127905e66f50efca4967
OliverGlandberger <oliverglandberger@gmail.com>
2018-02-14 14:42:54 +0100

    Added and implemented attack sounds.

--- LittleGame/AudioManager.cpp
+++ LittleGame/AudioManager.cpp
@@ -14,6 +14,28 @@ int AudioManager::init()
 	error += this->loadSound(std::string("include/Sound/beep3.wav"), SOUND::BEEP3);
 	error += this->loadSound(std::string("include/Sound/beep4.wav"), SOUND::BEEP4);
 
+	error += this->loadSound(std::string("include/Sound/EnemyAttack_1.wav"), SOUND::ENEMYATTACK_1);
+	error += this->loadSound(std::string("include/Sound/EnemyAttack_2.wav"), SOUND::ENEMYATTACK_2);
+	error += this->loadSound(std::string("include/Sound/EnemyAttack_3.wav"), SOUND::ENEMYATTACK_3);
+	error += this->loadSound(std::string("include/Sound/EnemyAttack_4.wav"), SOUND::ENEMYATTACK_4);
+	error += this->loadSound(std::string("include/Sound/EnemyAttack_5.wav"), SOUND::ENEMYATTACK_5);
+	error += this->loadSound(std::string("include/Sound/EnemyAttack_6.wav"), SOUND::ENEMYATTACK_6);
+	error += this->loadSound(std::string("include/Sound/EnemyAttack_7.wav"), SOUND::ENEMYATTACK_7);
+	error += this->loadSound(std::string("include/Sound/EnemyAttack_8.wav"), SOUND::ENEMYATTACK_8);
+	error += this->loadSound(std::string("include/Sound/EnemyAttack_9.wav"), SOUND::ENEMYATTACK_9);
+	error += this->loadSound(std::string("include/Sound/EnemyAttack_10.wav"), SOUND::ENEMYATTACK_10);
+	error += this->loadSound(std::string("include/Sound/EnemyAttack_11.wav"), SOUND::ENEMYATTACK_11);
+
+	error += this->loadSound(std::string("include/Sound/AttackGrunt_1.wav"), SOUND::ATTACKGRUNT_1);
+	error += this->loadSound(std::string("include/Sound/AttackGrunt_2.wav"), SOUND::ATTACKGRUNT_2);
+	error += this->loadSound(std::string("include/Sound/AttackGrunt_3.wav"), SOUND::ATTACKGRUNT_3);
+	error += this->loadSound(std::string("include/Sound/AttackGrunt_4.wav"), SOUND::ATTACKGRUNT_4);
+	error += this->loadSound(std::string("include/Sound/AttackGrunt_5.wav"), SOUND::ATTACKGRUNT_5);
+	error += this->loadSound(std::string("include/Sound/AttackGrunt_6.wav"), SOUND::ATTACKGRUNT_6);
+	error += this->loadSound(std::string("include/Sound/AttackGrunt_7.wav"), SOUND::ATTACKGRUNT_7);
+	error += this->loadSound(std::string("include/Sound/AttackGrunt_8.wav"), SOUND::ATTACKGRUNT_8);
+	error += this->loadSound(std::string("include/Sound/AttackGrunt_9.wav"), SOUND::ATTACKGRUNT_9);
+
 	error += this->mapMusic(std::string("include/Sound/OPM-OST.ogg"), MUSIC::ONEPUNCH);
 
 	this->adjustEffects(this->soundVolume);
