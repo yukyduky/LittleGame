9d1fd4f5f85657f43ddbb20e5ac98c3c953df63a
Heso113 <hampe_olsson91@hotmail.com>
2018-02-28 14:34:52 +0100

    merged with master

--- LittleGame/GamePlayState.cpp
+++ LittleGame/GamePlayState.cpp
@@ -419,7 +419,7 @@ void GamePlayState::update(GameManager * gm)
 	}
 
 	this->checkPlayerTileStatus();
-//	this->enemyManager.update();
+	this->enemyManager.update();
 	this->checkCollisions();
 }
 
